b2acb7fd4c001bf3a4f29d897c7687facfb2c55d
Jarmo Annunen <jannunen@koodiorja.com>
2020-02-24 16:29:33 +0200

    Managed to do a gridview in parts.

--- lib/screens/home/ProblemDetails.dart
+++ lib/screens/home/ProblemDetails.dart
@@ -6,7 +6,6 @@ import 'package:problemator/models/Problem.dart';
 import 'package:spinner_input/spinner_input.dart';
 
 class ProblemDetails extends StatefulWidget {
-
   final Problem problem;
   ProblemDetails({Key key, this.problem}) : super(key: key);
 
@@ -14,7 +13,6 @@ class ProblemDetails extends StatefulWidget {
 }
 
 class _ProblemDetailsState extends State<ProblemDetails> {
-
   ProblemDetailsBloc _bloc;
 
   final Problem problem;
@@ -25,11 +23,10 @@ class _ProblemDetailsState extends State<ProblemDetails> {
   final double fontSizeLarge = 40;
   final double fontSizeHuge = 80;
 
-
   double _tries = 0;
   String _gradeOpinion = null;
 
-  _ProblemDetailsState( this.problem );
+  _ProblemDetailsState(this.problem);
 
   void _setTries(tries) {
     setState(() {
@@ -37,202 +34,214 @@ class _ProblemDetailsState extends State<ProblemDetails> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title : Text('Problem '+this.problem.tag)),
-        body : GridView.count(
-        crossAxisCount: 2,
-        children : [
-          gradeCell(),likesCell(),
-          triesCell(),gradeOpinionCell(),
+        appBar: AppBar(title: Text('Problem ' + this.problem.tag)),
+        body: ListView(shrinkWrap: true, children: [
+          GridView.count(shrinkWrap: true, crossAxisCount: 2, children: [
+            gradeCell(), likesCell(),
+            triesCell(), gradeOpinionCell(),
+            doLikeCell(), toolsCell()
+            // Do betavideo adding later
+          ]),
           StreamBuilder<ApiResponse<Problem>>(
-            stream : _bloc.problemDetailsStream,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                switch (snapshot.data.status) {
-                  case Status.LOADING:
-                    return Text("Loading...");
-                  case Status.COMPLETED:
-                    return _buildLoadedDetails(snapshot.data.data);
-                  break;
-                  case Status.ERROR:
-                    return Text("Oi ei");
-
+              stream: _bloc.problemDetailsStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  switch (snapshot.data.status) {
+                    case Status.LOADING:
+                      return Text("Loading...");
+                    case Status.COMPLETED:
+                      return _buildLoadedDetails(snapshot.data.data);
+                      break;
+                    case Status.ERROR:
+                      return Text("Oi ei");
+                  }
                 }
-              } 
-              return Container();
-        }),
-          doLikeCell(),toolsCell()
-          // Do betavideo adding later
-        ])
-        );
+                return Container();
+              }),
+        ]));
   }
-  Container _buildLoadedDetails(Problem p) {
 
-    return Container(
-      child : GridView.count(
+  Widget _buildLoadedDetails(Problem p) {
+    return GridView.count(
+        shrinkWrap: true,
         crossAxisCount: 2,
-      children: <Widget>[
-          ascentsCell(p),opinionsCell(p),
-    ]));
+        children: <Widget>[
+          ascentsCell(p),
+          opinionsCell(p),
+        ]);
   }
 
   gradeCell() {
     return Padding(
-      padding: EdgeInsets.all(8.0), 
-      child :  Column(
-        mainAxisSize : MainAxisSize.min,
-        mainAxisAlignment : MainAxisAlignment.center,
-        children : [
-          Text(this.problem.gradename ?? "",
-                    style: TextStyle(fontSize: this.fontSizeHuge, fontWeight: FontWeight.bold),
-          ),
-          Text(this.problem.tag ?? "",
-                    style: TextStyle(fontSize: this.fontSizeTiny, fontWeight: FontWeight.bold, color : Colors.grey[600]),
-          )
-        ])
-      );
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                this.problem.gradename ?? "",
+                style: TextStyle(
+                    fontSize: this.fontSizeHuge, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                this.problem.tag ?? "",
+                style: TextStyle(
+                    fontSize: this.fontSizeTiny,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600]),
+              )
+            ]));
   }
 
   likesCell() {
     return Padding(
-      padding: EdgeInsets.all(8.0), 
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-      thumbsUbDownRow(),
-      Text('by '+this.problem.author ?? "",
-      style : TextStyle(fontSize: this.fontSizeSmall)),
-      Text(this.problem.addedrelative ?? "",
-      style : TextStyle(fontSize: this.fontSizeSmall)),
-    ])
-    );
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              thumbsUbDownRow(),
+              Text('by ' + this.problem.author ?? "",
+                  style: TextStyle(fontSize: this.fontSizeSmall)),
+              Text(this.problem.addedrelative ?? "",
+                  style: TextStyle(fontSize: this.fontSizeSmall)),
+            ]));
   }
 
   triesCell() {
     return Padding(
-      padding: EdgeInsets.all(8.0), 
-      child:   Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children : [SpinnerInput(
-                    spinnerValue: _tries,
-                    minValue: 0,
-                     middleNumberWidth: 50,
-                  middleNumberStyle: TextStyle(fontSize: this.fontSizeLarge),
-                    maxValue: 100,
-                    onChange: (newValue) {
-                      _setTries(newValue);
-                    },
-                  ),Text('Tries')],
-                ));
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinnerInput(
+              spinnerValue: _tries,
+              minValue: 0,
+              middleNumberWidth: 50,
+              middleNumberStyle: TextStyle(fontSize: this.fontSizeLarge),
+              maxValue: 100,
+              onChange: (newValue) {
+                _setTries(newValue);
+              },
+            ),
+            Text('Tries')
+          ],
+        ));
   }
 
   gradeOpinionCell() {
     return Padding(
-      padding: EdgeInsets.all(8.0), 
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-     children : [DropdownButton<String>(
-    value: this._gradeOpinion,
-    icon: Icon(Icons.arrow_downward),
-    iconSize: 30,
-    elevation: 20,
-    style: TextStyle(
-      color: Colors.black,
-      fontSize: this.fontSizeLarge,
-    ),
-    underline:       Container(
-    ),
-    onChanged: (String newValue) {
-      setState(() {
-        _gradeOpinion = newValue;
-      });
-    },
-    items: <String>['6C', '6C+', '7A', '7A+','7B','7B+','7C']
-      .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value ?? ""),
-        );
-      })
-      .toList(),
-  ),Text('Grade opinion')]
-    
-    ));
+        padding: EdgeInsets.all(8.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          DropdownButton<String>(
+            value: this._gradeOpinion,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 30,
+            elevation: 20,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: this.fontSizeLarge,
+            ),
+            underline: Container(),
+            onChanged: (String newValue) {
+              setState(() {
+                _gradeOpinion = newValue;
+              });
+            },
+            items: <String>['6C', '6C+', '7A', '7A+', '7B', '7B+', '7C']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value ?? ""),
+              );
+            }).toList(),
+          ),
+          Text('Grade opinion')
+        ]));
   }
 
   ascentsCell(Problem p) {
     return Padding(
-      padding: EdgeInsets.all(8.0), 
-      child :  Column(
-        mainAxisSize : MainAxisSize.min,
-        mainAxisAlignment : MainAxisAlignment.center,
-        children : [
-          Text(p.ascentcount ?? "",
-                    style: TextStyle(fontSize: this.fontSizeLarge, fontWeight: FontWeight.bold),
-          ),
-          Text("Global ascents",
-                    style: TextStyle(fontSize: this.fontSizeTiny, fontWeight: FontWeight.bold, color : Colors.grey[600]),
-          )
-        ])
-      );
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                p.ascentcount ?? "",
+                style: TextStyle(
+                    fontSize: this.fontSizeLarge, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Global ascents",
+                style: TextStyle(
+                    fontSize: this.fontSizeTiny,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600]),
+              )
+            ]));
   }
 
   opinionsCell(Problem p) {
     return Padding(
-      padding: EdgeInsets.all(8.0), 
-    child: Text('Opinions'));
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                p.ascentcount ?? "",
+                style: TextStyle(
+                    fontSize: this.fontSizeLarge, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Global ascents",
+                style: TextStyle(
+                    fontSize: this.fontSizeTiny,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600]),
+              )
+            ]));
   }
-  
+
   doLikeCell() {
-    return Padding(
-      padding: EdgeInsets.all(8.0), 
-    child: Text('Like'));
+    return Padding(padding: EdgeInsets.all(8.0), child: Text('Like'));
   }
 
   toolsCell() {
-    return Padding(
-      padding: EdgeInsets.all(8.0), 
-    child: Text('Tools'));
+    return Padding(padding: EdgeInsets.all(8.0), child: Text('Tools'));
   }
 
   thumbsUbDownRow() {
     return Row(
-       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-      thumbsUp(),
-      loves(),
-      thumbsDown()
-    ]);
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[thumbsUp(), loves(), thumbsDown()]);
   }
 
   thumbsUp() {
     return Row(children: <Widget>[
-      Text(this.problem.cLike ?? "",
-      style:  TextStyle(fontSize: 26)),
+      Text(this.problem.cLike ?? "", style: TextStyle(fontSize: 26)),
       Icon(FontAwesomeIcons.thumbsUp)
-    ],
-    mainAxisAlignment: MainAxisAlignment.spaceAround);
+    ], mainAxisAlignment: MainAxisAlignment.spaceAround);
   }
+
   loves() {
     return Row(children: <Widget>[
-      Text(this.problem.cLove ?? "",
-      style:  TextStyle(fontSize: 26)),
-      Icon(FontAwesomeIcons.heart,
-      color:  Colors.red[500],)
-    ],
-    mainAxisAlignment: MainAxisAlignment.spaceAround);
+      Text(this.problem.cLove ?? "", style: TextStyle(fontSize: 26)),
+      Icon(
+        FontAwesomeIcons.heart,
+        color: Colors.red[500],
+      )
+    ], mainAxisAlignment: MainAxisAlignment.spaceAround);
   }
+
   thumbsDown() {
     return Row(children: <Widget>[
-      Text(this.problem.cDislike ?? "",
-      style:  TextStyle(fontSize: 26)),
+      Text(this.problem.cDislike ?? "", style: TextStyle(fontSize: 26)),
       Icon(FontAwesomeIcons.thumbsDown)
-    ],
-    mainAxisAlignment: MainAxisAlignment.spaceAround);
-
+    ], mainAxisAlignment: MainAxisAlignment.spaceAround);
   }
 
   @override
@@ -241,6 +250,5 @@ class _ProblemDetailsState extends State<ProblemDetails> {
     // Find problem details
     _bloc = ProblemDetailsBloc();
     _bloc.fetchProblemDetails(this.problem.id);
-
   }
 }
